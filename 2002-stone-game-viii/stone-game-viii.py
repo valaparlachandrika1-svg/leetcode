2c5b71a58fe2c64948f4348ed266cf2842cf4c77
Chandrika Valaparla <valaparlachandrika1@gmail.com>
2026-08-24 20:01:29 +0530

    Time: 806 ms (81.82%) | Memory: 24.5 MB (18.18%) - LeetSync

---
+++ 2002-stone-game-viii/stone-game-viii.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def stoneGameVIII(self, stones):
+        """
+        :type stones: List[int]
+        :rtype: int
+        """
+        n = len(stones)
+
+       
+        prefix = [0] * n
+        prefix[0] = stones[0]
+
+        for i in range(1, n):
+            prefix[i] = prefix[i - 1] + stones[i]
+
+        
+        dp = prefix[n - 1]
+
+    
+        for i in range(n - 2, 0, -1):
+            dp = max(dp, prefix[i] - dp)
+        return dp
+
+
+        
