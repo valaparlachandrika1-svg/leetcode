c20d15f7014cda8a77d499e009c15129641f2710
Chandrika Valaparla <valaparlachandrika1@gmail.com>
2026-09-04 16:04:11 +0530

    Time: 2 ms (97.53%) | Memory: 12.5 MB (32.10%) - LeetSync

---
+++ 4284-smallest-stable-index-i/smallest-stable-index-i.py
@@ -0,0 +1,30 @@
+class Solution(object):
+
+    def firstStableIndex(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        
+        n = len(nums)
+
+        # Find suffix minimums
+        suffix_min = [0] * n
+        suffix_min[n - 1] = nums[n - 1]
+
+        for i in range(n - 2, -1, -1):
+            suffix_min[i] = min(nums[i], suffix_min[i + 1])
+
+        # Check each index
+        prefix_max = nums[0]
+
+        for i in range(n):
+            prefix_max = max(prefix_max, nums[i])
+
+            instability = prefix_max - suffix_min[i]
+
+            if instability <= k:
+                return i
+
+        return -1
